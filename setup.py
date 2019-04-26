e9468258a1a99eb04f43519af43d4598af62b05d
Adrian Sampson <adrian@radbox.org>
2019-04-26 09:28:30 -0400

    Version bump: 0.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='bibble',
-    version='0.0.1',
+    version='0.0.2',
     packages=['bibble'],
     include_package_data=True,
     install_requires=[
